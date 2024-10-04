ade714f937039c6345bc5e1339d7513ab788888f
Francisco Hanlly da Silva <hanlly.silvae@gmail.com>
2024-10-04 14:05:09 -0300

    update volume with ssh key

--- vars/nodeImageUpdate.groovy
+++ vars/nodeImageUpdate.groovy
@@ -8,18 +8,18 @@ def call(){
         VALUES_FILE="charts/values.yaml"
 
         apk add git openssh
-        ssh-keyscan github.com ${HOME}/.ssh/known_hosts
-         git clone git@github.com:frhanlly/simple-app-helm.git
-         cd simple-app-helm
-         git switch development
+        ssh-keyscan github.com > ${HOME}/.ssh/known_hosts
+        git clone git@github.com:frhanlly/simple-app-helm.git
+        cd simple-app-helm
+        git switch development
         
         git config --global user.email "jenkins@hanlly.local"
         git config --global user.name "Jenkins pipeline"
 
-         sed -i -E 's/tag:.+/tag: dev-${GIT_COMMIT:0:10}' ${VALUES_FILE}
+        sed -i -E 's/tag:.+/tag: dev-${GIT_COMMIT:0:10}' ${VALUES_FILE}
 
-         git commit -a -m "update image TAG to dev-${GIT_COMMIT:0:10}"
-         git push origin
+        git commit -a -m "update image TAG to dev-${GIT_COMMIT:0:10}"
+        git push origin
 
         '''
     }
